5bb5541f6ef639f8cbc35b25872b03785501c1f0
Lord Mico <michael.costas377@gmail.com>
2020-11-28 22:21:27 -0500

    Functions update - Added some test functions - Ackley function should have minimum at [0, 0] - Beale function should have a minimum at [3, 0.5]
    Signed-off-by: Lord Mico <michael.costas377@gmail.com>

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from GASolver import GASolver
+import math
 
 def test_function_1(x):
     return 5*x[0]+6*x[1]-3*x[2]+4*x[3]+5*x[4]-22*x[5]**2
@@ -7,14 +8,34 @@ def test_function_2(x):
     return (x[0]+x[1])/(3+x[0]**2+x[1]**2+x[0]*x[1])
 
 def test_function_3(x):
-    return 6*x[0]*x[1]*x[2]/(x[0]+2*x[1]+2*x[2])
+    return x[0] * x[1]
 
+def ackley_function(x):
+    a = 20
+    b = 0.2
+    c = 2*math.pi
+    d = 2
+    return -1 * a * math.exp(-1 * b * ((1/d) * (x[0] ** 2 + x[1] ** 2))**0.5) - math.exp((1/d)*(math.cos(c*x[0]) + math.cos(c*x[1]))) + a + math.exp(1)
+
+def beale_function(x):
+    return (1.5 - x[0] + x[0] * x[1]) ** 2 + (2.25 - x[0] + x[0] * x[1] ** 2) ** 2 + (2.625 - x[0] + x[0] * x[1] ** 3) ** 2
+
+# Example 1: Non-converging Question
 #solver = GASolver(test_function_1,6)
 #solver.solve((-5,5),50,20)
 
-solver2 = GASolver(test_function_2,2, False)
-solver2.solve([-5,5],50,25)
+# Example 2: Converging Question
+#solver2 = GASolver(test_function_2,2, False)
+#solver2.solve([-5,5],50,25)
 
+# Example 3: Converging Bounded Arora 3.9
 #solver3 = GASolver(test_function_3,3)
 #solver3.solve([0,5],10,5)
 
+# Example 4: Ackley Function Orig 
+#solver_ackley = GASolver(ackley_function,2,False)
+#solver_ackley.solve([-10, 10],50,15)
+
+# Example 5: Beale Function
+#solver_beale = GASolver(beale_function, 2, False)
+#solver_beale.solve([-10, 10], 75, 15, 50)
